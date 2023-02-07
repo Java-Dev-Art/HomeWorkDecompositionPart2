8514308b3d8839182eb6228acc9502cad792354a
Artur Markouski <veles.mk.12@gmail.com>
2023-02-07 23:19:56 +0100

    fix:update class

--- src/main/java/TaskArmstrongNumber.java
+++ src/main/java/TaskArmstrongNumber.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class TaskArmstrongNumber {
+public class TaskArmstrongNumber {
+
+    public boolean isArmstrong(int number) {
+        int sum = 0;
+        for (char c : separateNumber(number)) {
+            sum += Math.pow(Integer.parseInt(String.valueOf(c)), separateNumber(number).length);
+        }
+        return isArmstrong(sum, number);
+    }
+
+    private char[] separateNumber(int number) {
+        String stringArmstrong = String.valueOf(number);
+        return stringArmstrong.toCharArray();
+    }
+
+    private boolean isArmstrong(int firstNumber, int secondNumber) {
+        if (firstNumber == secondNumber) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
+
--- src/test/TestTaskArmstrongNumber.java
+++ src/test/TestTaskArmstrongNumber.java
@@ -1,2 +1,36 @@
-package PACKAGE_NAME;public class TestTaskArmstrongNumber {
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+@RunWith(Parameterized.class)
+public class TestTaskArmstrongNumber {
+    private final boolean expected;
+    private final int number;
+
+    public TestTaskArmstrongNumber(boolean expected, int number) {
+        this.expected = expected;
+        this.number = number;
+    }
+    @Parameterized.Parameters
+    public static Collection<Object[]> setParameter(){
+        return Arrays.asList(new Object[][]{
+                {true,1},
+                {true,2},
+                {true,153},
+                {false,152},
+                {false,100},
+                {true,370},
+                {true,8208},
+                {false,1200},
+                {true,1_741_725},
+        });
+    }
+    @Test
+    public void setTest(){
+        TaskArmstrongNumber armstrongNumber = new TaskArmstrongNumber();
+        Assert.assertEquals(expected,armstrongNumber.isArmstrong(number));
+    }
 }
